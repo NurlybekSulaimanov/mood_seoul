6693c743a9f2fa21640803660adbce13a7f022ea
NurlybekSulaimanov <kaistnurlybek@gmail.com>
2024-01-26 14:00:40 +0900

    hashrouter add

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <GoogleOAuthProvider clientId="650591104699-62if1fsq2gk123vai06hn9gvnfffvnd5.apps.googleusercontent.com">
         <App />
       </GoogleOAuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
